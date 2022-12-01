b2de6ed8de1983a44986ebac38247a18f09790f7
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-01 16:22:56 +0600

    Add spotify credentials as variables

--- 46-Create a Spotify Playlist using the Musical Time Machine/main.py
+++ 46-Create a Spotify Playlist using the Musical Time Machine/main.py
@@ -1,23 +1,30 @@
 from bs4 import BeautifulSoup
 import requests
+import os
 
-# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
 
-response = requests.get("https://www.billboard.com/charts/hot-100/2012-11-09") #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date) #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
 # print(response.text)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
-print(song_names)
+# print(song_names)
 
-music_list = []
+final_song_list = []
 for song in song_names:
     text = song.getText()
-    print(text)
+    # print(text)
     text = text.strip()
-    print(text)
-    music_list.append(text)
-print(music_list)
+    # print(text)
+    final_song_list.append(text)
+print(final_song_list)
+
+
+# Spotify credentials:
+ClientID = os.environ.get("ID")
+ClientSecret = os.environ.get("Secret")
+
 
 
 
