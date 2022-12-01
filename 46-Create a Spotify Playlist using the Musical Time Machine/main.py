f3e08e926d5be344445219acca2d64db77d914d7
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-01 17:02:45 +0600

    Comment

--- 46-Create a Spotify Playlist using the Musical Time Machine/main.py
+++ 46-Create a Spotify Playlist using the Musical Time Machine/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 import os
 
+#Create list of songs from URL:
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
 
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date) #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
