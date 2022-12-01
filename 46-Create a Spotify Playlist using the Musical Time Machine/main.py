6468b219f0288675962daecaef735a63675fa01c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-01 14:09:05 +0600

    Get request

--- 46-Create a Spotify Playlist using the Musical Time Machine/main.py
+++ 46-Create a Spotify Playlist using the Musical Time Machine/main.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
+
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date) #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
+print(response.text)
