a9d914e2df7a1925a16a43825c284b18c81721ca
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-01 15:41:18 +0600

    Curate music list of top 100 of specified date

--- 46-Create a Spotify Playlist using the Musical Time Machine/main.py
+++ 46-Create a Spotify Playlist using the Musical Time Machine/main.py
@@ -1,7 +1,25 @@
 from bs4 import BeautifulSoup
 import requests
 
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
+# date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ") #enter 2012-11-09
 
-response = requests.get("https://www.billboard.com/charts/hot-100/" + date) #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
-print(response.text)
+response = requests.get("https://www.billboard.com/charts/hot-100/2012-11-09") #becomes https://www.billboard.com/charts/hot-100/2012-11-09/
+# print(response.text)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+song_names = soup.find_all(name="h3", id="title-of-a-story", class_="a-no-trucate")
+print(song_names)
+
+music_list = []
+for song in song_names:
+    text = song.getText()
+    print(text)
+    text = text.strip()
+    print(text)
+    music_list.append(text)
+print(music_list)
+
+
+
+
+
