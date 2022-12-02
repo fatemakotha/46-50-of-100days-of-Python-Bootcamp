f3e55b4d861c89ca68b1af960c1840599d516dd5
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-02 16:52:26 +0600

    Close the active tab

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
@@ -1,4 +1,11 @@
 from selenium import webdriver
 
-chrome_driver_path = "C:\Users\fatem\OneDrive\Desktop\100 days of python bootcamp\DAY 46\chromedriver.exe"
+chrome_driver_path = "C:/Users/fatem/OneDrive/Desktop/100 days of python bootcamp/DAY 46/chromedriver.exe"
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.amazon.com") #In mac, it won't run. So go to apple symbol: system preferences: security and privacy
+                                    # It will say "chromedriver" was blocked and we click "allow anyway.
+                                    #Now, when run it will open a popup with an "open" button. click open.
+driver.close() #Closes the active tab
+driver.quit()
+
