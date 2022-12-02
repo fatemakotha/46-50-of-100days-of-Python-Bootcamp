9815d52f1d3c0f910ee966801b656e860348885c
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-02 16:42:25 +0600

    Add name of chromedriver.exe to the end of the path

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
@@ -1,1 +1,1 @@
-chrome_driver_path = "C:\Users\fatem\OneDrive\Desktop\100 days of python bootcamp\DAY 46"
+chrome_driver_path = "C:\Users\fatem\OneDrive\Desktop\100 days of python bootcamp\DAY 46\chromedriver.exe"
