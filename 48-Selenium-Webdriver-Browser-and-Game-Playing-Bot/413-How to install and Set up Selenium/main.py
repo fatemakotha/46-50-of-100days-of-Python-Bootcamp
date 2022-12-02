d3fdf121558ddf5b7674ec938144a9b4c722d737
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-02 16:44:07 +0600

    Create a driver from webdriver.Chrome() object

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
@@ -1,1 +1,4 @@
+from selenium import webdriver
+
 chrome_driver_path = "C:\Users\fatem\OneDrive\Desktop\100 days of python bootcamp\DAY 46\chromedriver.exe"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
