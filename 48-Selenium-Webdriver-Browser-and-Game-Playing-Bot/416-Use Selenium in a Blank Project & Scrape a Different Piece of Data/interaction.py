5d89e23b34fe49b5a272e0fbaa9e14807e295674
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-03 20:16:33 +0600

    Another example of CSS Selectors

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/416-Use Selenium in a Blank Project & Scrape a Different Piece of Data/interaction.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/416-Use Selenium in a Blank Project & Scrape a Different Piece of Data/interaction.py
@@ -9,5 +9,15 @@ options. add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://en.wikipedia.org/wiki/Main_Page")
 
-num_articles = driver.find_element(By.CSS_SELECTOR, ".mp-box #articlecount a") #***taps into the returns the no of articles that is shown at the top of the page
-print(num_articles.text) #6,583,142
+
+#Combine class, ID and name in CSS Selecttor
+#<div id="mp-topbanner" class="mp-box">_____(1)
+# <div id="articlecount">_____(1)
+# <a href="/wiki/Special:Statistics" title="Special:Statistics">6,583,131</a>_____(1)
+num_articles = driver.find_element(By.CSS_SELECTOR, ".mp-box #articlecount a") #_____(1)
+print(num_articles.text) #6,583,142 _____(1)
+
+#Just another example of CSS Selectors
+vvv = driver.find_element(By.CSS_SELECTOR, ".mp-box #articlecount") #***taps into the returns the no of articles that is shown at the top of the page
+print(vvv.text) #6,583,149 articles in English
+
