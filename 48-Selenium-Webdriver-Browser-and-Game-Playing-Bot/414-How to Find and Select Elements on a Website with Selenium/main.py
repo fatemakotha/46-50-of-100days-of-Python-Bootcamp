c784fff9562e0744d57910037d9785a41f86f59b
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-03 14:55:27 +0600

    driver.get() opens up the url assigned

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/414-How to Find and Select Elements on a Website with Selenium/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/414-How to Find and Select Elements on a Website with Selenium/main.py
@@ -6,7 +6,9 @@ from selenium import webdriver
 #Set url to open up:
 chrome_driver_path = "C:/Users/fatem/OneDrive/Desktop/100 days of python bootcamp/DAY 46/chromedriver.exe"
 
+
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
+#driver.get() opens up the url assigned:
 driver.get("https://www.amazon.com/Instant-Pot-Pressure-Steamer-Sterilizer/dp/B08PQ2KWHS/ref=sr_1_2_sspa?crid=3PS0AQ9GG8NE&keywords=Instant+Pot+Duo+Evo&qid=1669968418&sprefix=instant+pot+duo+evo%2Caps%2C392&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyNTYzWU85WFJUQjlSJmVuY3J5cHRlZElkPUEwMTIwOTkxMVBCQU1GSlY0WkY5SSZlbmNyeXB0ZWRBZElkPUEwMzg0OTAxMk9aTTk5OTJORzU0NCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=")
 
 
