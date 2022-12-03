cf3883b86f5e5b219b103207d20dd0b645744fce
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-03 14:48:57 +0600

    From selenium import all

---
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/414-How to Find and Select Elements on a Website with Selenium/main.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium import *
+
+chrome_driver_path = "C:/Users/fatem/OneDrive/Desktop/100 days of python bootcamp/DAY 46/chromedriver.exe"
+
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get("https://www.amazon.com") #In mac, it won't run. So go to apple symbol: system preferences: security and privacy
+                                    # It will say "chromedriver" was blocked and we click "allow anyway.
+                                    #Now, when run it will open a popup with an "open" button. click open.
+# driver.close() #Closes the active tab
+# driver.quit() #Closes the entire window
