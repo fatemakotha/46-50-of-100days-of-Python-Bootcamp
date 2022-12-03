e6e8ab5ef9690e21fd8cd699c55f0ece5391ea09
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-03 14:37:07 +0600

    How to Find and Select Elements on a Website with Selenium

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/413-How to install and Set up Selenium/main.py
@@ -6,6 +6,6 @@ driver = webdriver.Chrome(executable_path=chrome_driver_path)
 driver.get("https://www.amazon.com") #In mac, it won't run. So go to apple symbol: system preferences: security and privacy
                                     # It will say "chromedriver" was blocked and we click "allow anyway.
                                     #Now, when run it will open a popup with an "open" button. click open.
-driver.close() #Closes the active tab
-driver.quit() #Closes the entire window
+# driver.close() #Closes the active tab
+# driver.quit() #Closes the entire window
 
