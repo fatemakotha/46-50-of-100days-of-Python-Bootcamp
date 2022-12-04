71088cabb3979b8b40c2ee21a243e913e251c84e
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-04 17:20:59 +0600

    Import selenium

---
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/418-The Cookie Clicker Project/main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+#To press enter and everything:
+from selenium.webdriver.common.keys import Keys
+
+
+
