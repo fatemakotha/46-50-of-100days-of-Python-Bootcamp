93514a0bd716d4a1b56d51be22d7556ec9ce9ada
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-04 17:38:14 +0600

    Create a list of item_ids

--- 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/418-The Cookie Clicker Project/main.py
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/418-The Cookie Clicker Project/main.py
@@ -16,5 +16,24 @@ driver = webdriver.Chrome(chrome_options=options)
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 
 #Get cookie to click on
-cookie = driver.find_element(By.ID, "cookie")
+# cookie = driver.find_element(By.ID, "cookie")
+
+
+#Get upgrade item ids:
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+print(items) #[<selenium.webdriver.remote.webelement.WebElement (session="3e543f33de0881f92e542becce1ab368", element="87cc12f5-ee08-423c-9e9a-2c1e7b99f154")>, <selenium.webdriver.remote.webelement.WebElement (session="3e54
+
+# item = driver.find_element(By.CSS_SELECTOR, "#store div")
+# print(item)
+
+
+#Create a list of item_ids:
+# item_ids = [item.get_attribute("id") for item in items] #----OR------#
+item_ids = []
+for item in items:
+    attribute = item.get_attribute("id")
+    item_ids.append(attribute)
+    # print(attribute)
+print(item_ids) #['buyCursor', 'buyGrandma', 'buyFactory', 'buyMine', 'buyShipment', 'buyAlchemy lab', 'buyPortal', 'buyTime machine', 'buyElder Pledge']
+
 
