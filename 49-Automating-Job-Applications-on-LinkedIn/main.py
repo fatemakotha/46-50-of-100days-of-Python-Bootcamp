833651dc4c31e0631faf15f2eb9fca7d7ec86635
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-05 14:25:56 +0600

    Click easy-apply button

--- 49-Automating-Job-Applications-on-LinkedIn/main.py
+++ 49-Automating-Job-Applications-on-LinkedIn/main.py
@@ -6,12 +6,13 @@ from selenium.common.exceptions import TimeoutException
 #To press enter and everything:
 from selenium.webdriver.common.keys import Keys
 import os
+import time
 
 username = os.environ.get("Lk_email")
 password = os.environ.get("Lk_password")
 
 
-
+#LOGGING INTO LINKED IN USING THE LINK : -----------------------------------------------------------------------------------------------------------
 #Open the desired website:
 options = webdriver. ChromeOptions()
 options. add_experimental_option("detach", True)
@@ -20,6 +21,19 @@ driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3372310590&f_AL=t
 #Click on sign-in:
 signin_btn = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
 signin_btn.click()
+##Wait for the next page to load.
+# time.sleep(5) **************************************
 #Fill the username:
 user = driver.find_element(By.ID, "username")
 user.send_keys(username)
+#Fill the password:
+passw = driver.find_element(By.ID, "password")
+passw.send_keys(password)
+#Click sign in button:
+submit = driver.find_element(By.CSS_SELECTOR, ".login__form_action_container button")
+submit.click()
+#------------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Click easy-apply button:
+easyapply_btn = driver.find_element(By.CSS_SELECTOR, ".mt5 .display-flex button")
+easyapply_btn.click()
