db0f9d90dceb0928993a9c55e312d068aa217011
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-05 15:25:35 +0600

    Complete

---
+++ 48-Selenium-Webdriver-Browser-and-Game-Playing-Bot/417-How to Automate Filling Out Forms and Clicking Buttons with Selenium/424. Apply for all the jobs.py
@@ -0,0 +1,83 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+#To press enter and everything:
+from selenium.webdriver.common.keys import Keys
+import os
+import time
+
+username = os.environ.get("Lk_email")
+password = os.environ.get("Lk_password")
+PHONE = "01210121012"
+
+#LOGGING INTO LINKED IN USING THE LINK : -----------------------------------------------------------------------------------------------------------
+#Open the desired website:
+options = webdriver. ChromeOptions()
+options. add_experimental_option("detach", True)
+driver = webdriver.Chrome(chrome_options=options)
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3372310590&f_AL=true&f_E=1&keywords=python%20intern")
+#Click on sign-in:
+signin_btn = driver.find_element(By.CLASS_NAME, "nav__button-secondary")
+signin_btn.click()
+##Wait for the next page to load.
+time.sleep(5)
+#Fill the username:
+user = driver.find_element(By.ID, "username")
+user.send_keys(username)
+#Fill the password:
+passw = driver.find_element(By.ID, "password")
+passw.send_keys(password)
+#Click sign in button:
+submit = driver.find_element(By.CSS_SELECTOR, ".login__form_action_container button")
+submit.click()
+#--------------------------------------------------------------------------------------------------------------------------------------
+
+all_listings = driver.find_elements(By.CSS_SELECTOR, ".job-card-container--clickable")
+
+for listing in all_listings:
+    print("called")
+    listing.click()
+    time.sleep(2)
+
+    # Try to locate the apply button, if can't locate then skip the job.
+    try:
+        apply_button = driver.find_element(By.CSS_SELECTOR, ".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        # If phone field is empty, then fill your phone number.
+        phone = driver.find_element(By.CLASS_NAME, "fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        submit_button = driver.find_element(By.CSS_SELECTOR, "footer button")
+
+        # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_elements(By.CLASS_NAME, "artdeco-modal__confirm-dialog-btn")[1]
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+
+        # Once application completed, close the pop-up window.
+        time.sleep(2)
+        close_button = driver.find_element(By.CLASS_NAME, "artdeco-modal__dismiss")
+        close_button.click()
+
+    # If already applied to job or job is no longer accepting applications, then skip.
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
+
+time.sleep(5)
+driver.quit()
+
+
+
