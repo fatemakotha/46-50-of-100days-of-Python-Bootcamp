e2976baacc85a699123dd49ad96becd2a1e888dc
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-07 20:05:24 +0600

    Set facebook email and password as env variables

--- 50-Auto-Tinder-Swiping-Bot/main.py
+++ 50-Auto-Tinder-Swiping-Bot/main.py
@@ -8,7 +8,9 @@ from selenium.webdriver.common.keys import Keys
 import os
 import time
 
-
+#Set facebook email and password as env variables:
+FB_EMAIL = os.environ.get("fb_email")
+FB_PASSWORD = os.environ.get("fb_pass")
 
 #LOGGING INTO LINKED IN USING THE LINK : -----------------------------------------------------------------------------------------------------------
 #Open the desired website:
@@ -16,5 +18,13 @@ options = webdriver. ChromeOptions()
 options. add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://tinder.com/")
+time.sleep(5)
 
-#Login using Facebook
+
+#Login using Facebook:
+login = driver.find_element(By.XPATH, '//*[@id="q-401777178"]/div/div[1]/div/main/div[1]/div/div/div/div/div[3]/div/div[2]/button')
+login.click()
+
+fb_login = driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
