0d3e508a46473fd4f287051c67af5695f86103e2
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-07 21:19:34 +0600

    Complete

--- 50-Auto-Tinder-Swiping-Bot/main.py
+++ 50-Auto-Tinder-Swiping-Bot/main.py
@@ -35,8 +35,8 @@ fb_login_window = driver.window_handles[1]
 driver.switch_to.window(fb_login_window)
 print(driver.title)
 
-email = driver.find_element_by_xpath('//*[@id="email"]')
-password = driver.find_element_by_xpath('//*[@id="pass"]')
+email = driver.find_element(By.XPATH, '//*[@id="email"]')
+password = driver.find_element(By.XPATH, '//*[@id="pass"]')
 
 email.send_keys(FB_EMAIL)
 password.send_keys(FB_PASSWORD)
@@ -45,12 +45,12 @@ password.send_keys(Keys.ENTER)
 driver.switch_to.window(base_window)
 print(driver.title)
 
-time.sleep()
-allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+time.sleep(5)
+allow_location_button = driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
 allow_location_button.click()
-notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button = driver.find_element(By.XPATH, '//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
 notifications_button.click()
-cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies = driver.find_element(By.XPATH, '//*[@id="content"]/div/div[2]/div/div/div[1]/button')
 cookies.click()
 
 #Tinder free tier only allows 100 "Likes" per day. If you have a premium account, feel free to change to a while loop.
@@ -61,14 +61,13 @@ for n in range(100):
 
     try:
         print("called")
-        like_button = driver.find_element_by_xpath(
-            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button = driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
         like_button.click()
 
     #Catches the cases where there is a "Matched" pop-up in front of the "Like" button:
     except ElementClickInterceptedException:
         try:
-            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup = driver.find_element(By.CSS_SELECTOR, ".itsAMatch a")
             match_popup.click()
 
         #Catches the cases where the "Like" button has not yet loaded, so wait 2 seconds before retrying.
