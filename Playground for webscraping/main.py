91468f14dbf7ab6a0c31b2260e5e1d6625aecce1
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-09 13:35:22 +0600

    Tapping into a button

--- Playground for webscraping/main.py
+++ Playground for webscraping/main.py
@@ -13,6 +13,7 @@ options = webdriver. ChromeOptions()
 options. add_experimental_option("detach", True)
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("https://www.python.org/")
+
 #FINDING A HEADING:_______________________1
 # <div class="list-widgets row">
 #     <div class ="list-widgets row" >
@@ -23,3 +24,10 @@ title = driver.find_element(By.CSS_SELECTOR, ".shrubbery h2") #gets the tite "La
 print(f"title is {title.text}") #title is Latest News
 title_x = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[3]/div[1]/div/h2')
 print(f"title is {title_x.text}") #title is Latest News
+print("-------------------------------------------------------------------------------------------------------------------------------")
+
+#FINDING A BUTTOM:_______________________2
+#<div class="options-bar-container do-not-print">
+#   <a href="https://psfmember.org/civicrm/contribute/transact/?reset=1&id=2" class="donate-button"> Donate </a>
+donate_button = driver.find_element(By.CSS_SELECTOR, ".options-bar-container a") #Use the first word of class to target as CSS ***
+donate_button.click()
