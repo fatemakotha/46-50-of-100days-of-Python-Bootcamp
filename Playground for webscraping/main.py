d6ad650840e0211912d81c4b6b88d131f4d261cd
fatemakotha <fatema.alam.kotha@gmail.com>
2022-12-09 14:06:01 +0600

    Tapping into a textbox

--- Playground for webscraping/main.py
+++ Playground for webscraping/main.py
@@ -16,14 +16,16 @@ driver.get("https://www.python.org/")
 
 #FINDING A HEADING:_______________________1
 # <div class="list-widgets row">
-#     <div class ="list-widgets row" >
-#         <div class="list-widgets row">
+#     <div class ="medium-widget blog-widget" >
+#         <div class="shrubbery">
 #             <h2 class="widget-title">
 #                 <span></span> Lastest News: </h2>
 title = driver.find_element(By.CSS_SELECTOR, ".shrubbery h2") #gets the tite "Latest News"
 print(f"title is {title.text}") #title is Latest News
 title_x = driver.find_element(By.XPATH, '//*[@id="content"]/div/section/div[3]/div[1]/div/h2')
 print(f"title is {title_x.text}") #title is Latest News
+title_y = driver.find_element(By.CSS_SELECTOR, ".medium-widget .shrubbery .widget-title")
+print(f"title is {title_y.text}") #title is Latest News
 print("-------------------------------------------------------------------------------------------------------------------------------")
 
 #FINDING A BUTTOM:_______________________2
@@ -34,4 +36,20 @@ print("-------------------------------------------------------------------------
 # donate_button.click() #WORKS
 # donate_button_x = driver.find_element(By.CSS_SELECTOR, ".options-bar-container .donate-button") #Use first words of 2 classes
 # donate_button_x.click() #WORKS
-print("-------------------------------------------------------------------------------------------------------------------------------")
+#print("-------------------------------------------------------------------------------------------------------------------------------")
+
+
+#FINDING A TEXTBOX TO TYPE IN:_______________________3
+# <form class="search-the-site" action="/search/" method="get">
+#   <fieldset title="Search Python.org">
+#       <span></span>
+#           <label class="screen-reader-text" for="id-search-field"> Search This Site </label>
+#               <input id="id-search-field" name="q" type="search" role="textbox" class="search-field" placeholder="Search" value tabindex="1">
+textbox = driver.find_element(By.CSS_SELECTOR, ".search-the-site fieldset .search-field") #*****
+textbox.send_keys("Look for xyz") #Works
+# textbox_x = driver.find_element(By.CSS_SELECTOR, ".screen-reader-text input")
+# textbox_x.send_keys("hhhh") #DOES NOT WORK
+# textbox_x = driver.find_element(By.CSS_SELECTOR, ".screen-reader-text .search-field")
+# textbox_x.send_keys("hhhh")#DOES NOT WORK
+#print("-------------------------------------------------------------------------------------------------------------------------------")
+
